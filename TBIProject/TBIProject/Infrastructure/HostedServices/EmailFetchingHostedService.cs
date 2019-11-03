64a728647a16136fca2479f4c019642604236566
tomas <tomastomov@yahoo.com>
2019-11-03 14:57:49 +0200

    Added HostedService

---
+++ TBIProject/TBIProject/Infrastructure/HostedServices/EmailFetchingHostedService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TBIProject.Services.Contracts;
+
+namespace TBIProject.Infrastructure.HostedServices
+{
+    public class EmailFetchingHostedService : IHostedService
+    {
+        private readonly IEmailService service;
+
+        public EmailFetchingHostedService(IEmailService service)
+        {
+            this.service = service;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
