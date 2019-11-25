43ffe0aefb2249a6091011acbc8f8e459ab0ce0d
Truesdae <kaloyan.krasimiroff@gmail.com>
2019-11-25 18:22:14 +0200

    ProcessEmailUpdateShouldPassOnStatusAccepted Tested

--- TBIProject/TBIProject.Tests/EmailProcessingService_Should.cs
+++ TBIProject/TBIProject.Tests/EmailProcessingService_Should.cs
@@ -361,7 +361,7 @@ namespace TBIProject.Tests
         }
 
         [TestMethod]
-        public async Task ProcessEmailUpdateShouldPass()
+        public async Task ProcessEmailUpdateShouldPassOnStatusOpen()
         {
             var options = new DbContextOptionsBuilder<TBIContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
 
@@ -474,6 +474,70 @@ namespace TBIProject.Tests
                 var sut = new EmailProcessingService(assertContext, encrypter.Object, userManager.Object, validator.Object, emailService);
                 var executionResult = await sut.ProcessEmailUpdate(updateParameters);
                 Assert.AreEqual(false, executionResult);
+            }
+        }
+
+        [TestMethod]
+        public async Task ProcessEmailUpdateShouldPassOnStatusAccepted()
+        {
+            var options = new DbContextOptionsBuilder<TBIContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            var userStore = new Mock<IUserStore<User>>();
+            var userManager = new Mock<UserManager<User>>(userStore.Object, null, null, null, null, null, null, null, null);
+
+            var encrypter = new Mock<IEncrypter>();
+            var validator = new Mock<IValidator>();
+            var emailService = new Mock<IEmailService>().Object;
+
+            var date = DateTime.Now;
+            var email = new Application
+            {
+                Id = 1,
+                Received = date,
+                Email = "kaloyan@abv.bg",
+                ApplicationStatus = ApplicationStatus.Open,
+                Body = "test text",
+                OperatorId = "1",
+                LastChange = date,
+                EGN= "1111111111",
+                Phone= "0876281932"
+            };
+            var updateParameters = new EmailUpdateModel
+            {
+                EmailId = 1,
+                LoggedUserUsername = "testUser",
+                Amount = 23,
+                NewStatus = "Accepted",               
+                FullName = "Ivan Petrov"
+            };
+
+            var user = new User { Id = "1", UserName = "testUser" };
+            var registeredUser = new User { Id = "2", UserName = "kaloyan@abv.bg" };
+
+            userManager.Setup(g => g.FindByNameAsync("testUser")).Returns(Task.FromResult(user));
+            userManager.Setup(g => g.FindByEmailAsync("kaloyan@abv.bg")).Returns(Task.FromResult(registeredUser));
+
+            userManager.Setup(g => g.IsInRoleAsync(user, "Operator")).Returns(Task.FromResult(true));
+
+            encrypter.Setup(d => d.Decrypt("kaloyan@abv.bg")).Returns("<kaloyan@abv.bg>");
+
+            validator.Setup(d => d.ValidateEGN("1111111111")).Returns(Task.FromResult(true));
+            validator.Setup(d => d.ValidatePhone("0876281932")).Returns(Task.FromResult(true));
+
+            validator.Setup(d => d.ValidateName("Ivan Petrov")).Returns(Task.FromResult(true));
+
+            using (var arrangeContex = new TBIContext(options))
+            {
+                arrangeContex.Applications.Add(email);
+                await arrangeContex.SaveChangesAsync();
+            }
+            using (var assertContext = new TBIContext(options))
+            {
+                var sut = new EmailProcessingService(assertContext, encrypter.Object, userManager.Object, validator.Object, emailService);
+                var executionResult = await sut.ProcessEmailUpdate(updateParameters);
+                Assert.AreEqual(true, executionResult);
+                Assert.AreEqual(1, await assertContext.Loans.CountAsync());
+                Assert.AreEqual(2, await assertContext.Users.CountAsync());
             }
         }
     }
