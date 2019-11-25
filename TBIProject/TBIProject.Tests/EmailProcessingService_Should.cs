5555e45010a149d3c6bdff79a2fedaa1ea85a7ec
Truesdae <kaloyan.krasimiroff@gmail.com>
2019-11-25 15:24:02 +0200

    Test project added

---
+++ TBIProject/TBIProject.Tests/EmailProcessingService_Should.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TBIProject.Tests
+{
+    [TestClass]
+    public class EmailProcessingService_Should
+    {
+    }
+}
